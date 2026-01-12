e97e44f9dacb567d9db52d2f686e5e51fedfe13b
Rick_Astley2 <ariknutson1@gmail.com>
2026-01-12 09:43:19 -0600

    Got the 16*16 grid to work

--- script.js
+++ script.js
@@ -2,20 +2,24 @@ const grid = document.querySelector("#gridContainer");
 
 function gridCreator(gridSize) {
   if (gridSize <= 100) {
-    for (i = 0; i < gridSize; i++) {
-      const divSquare = document.createElement("div");
-      divSquare.classList.add("divSquare");
-      divSquare.textContent = " ";
-      
-      divSquare.addEventListener("mouseover", () => divSquare.style.backgroundColor = "black");
-      
-      grid.appendChild(divSquare);
+    for (j = 0; j < gridSize; j++) {
+      const rowGrid = document.createElement("div");
+      for (i = 0; i < gridSize; i++) {
+        const divSquare = document.createElement("div");
+        divSquare.classList.add("divSquare");
+        divSquare.textContent = " ";
+
+        divSquare.addEventListener(
+          "mouseover",
+          () => (divSquare.style.backgroundColor = "black"),
+        );
+
+        rowGrid.appendChild(divSquare);
+      }
+      grid.appendChild(rowGrid)
     }
   }
 }
 
-
 let gridSize = 16;
 gridCreator(gridSize);
-
-
