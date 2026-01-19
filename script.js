58f1fc4b2a5ca86aeab3958a2332d4433b0d1a89
Rick_Astley2 <ariknutson1@gmail.com>
2026-01-18 19:46:24 -0600

    Finished grid
    Now adding the extra credit stuff it talked about
    
    Need to commit more

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const grid = document.querySelector("#gridContainer");
+const submitBtn = document.querySelector("#submitBtn");
+const input = document.querySelector("input");
+
+function submit(e) {
+  e.preventDefault();
+
+  let size = Number(input.value);
+
+  if (size > 0 && size <= 100) {
+    grid.innerHTML = "";
+    gridCreator(size);
+  }
+  input.value = "";
+  input.focus();
+}
 
 function gridCreator(gridSize) {
   if (gridSize <= 100) {
-    for (j = 0; j < gridSize; j++) {
+    for (let j = 0; j < gridSize; j++) {
       const rowGrid = document.createElement("div");
-      for (i = 0; i < gridSize; i++) {
+      rowGrid.classList.add("rowGrid")
+      let squareSize = 940 / gridSize;
+      for (let i = 0; i < gridSize; i++) {
         const divSquare = document.createElement("div");
         divSquare.classList.add("divSquare");
-        divSquare.textContent = " ";
+        divSquare.textContent = "";
+        divSquare.style.width = `${squareSize}px`;
+        divSquare.style.height = `${squareSize}px`;
 
         divSquare.addEventListener(
           "mouseover",
@@ -16,10 +35,10 @@ function gridCreator(gridSize) {
 
         rowGrid.appendChild(divSquare);
       }
-      grid.appendChild(rowGrid)
+      grid.appendChild(rowGrid);
     }
   }
 }
 
-let gridSize = 16;
-gridCreator(gridSize);
+gridCreator(16);
+submitBtn.addEventListener("click", submit);
